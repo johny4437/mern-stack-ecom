1fd1c78ea1f2f06b66beab67c072b12b57fbee38
johny anastacio <johnyanastaciods@gmail.com>
2020-07-13 21:50:28 -0300

    FRONTEND

--- frontend/src/core/CheckBox.js
+++ frontend/src/core/CheckBox.js
@@ -1,10 +1,26 @@
 import React,{useState, useEffect} from 'react'
 
-function CheckBox({categories}) {
+function CheckBox({categories, handleFilters}) {
+
+     const [checked, setChecked] = useState([]);
+     const handleToggle = c => () =>{
+            const currentCategoryId = checked.indexOf(c);
+            const newCategoryId = [...checked];
+
+            if( currentCategoryId === -1) {
+                newCategoryId.push(c)
+            }else{
+                newCategoryId.splice(currentCategoryId, 1)
+            }
+      //  console.log(newCategoryId)
+        setChecked(newCategoryId)
+        handleFilters(newCategoryId);
+        
+     }
     return (
         categories.map((c, i) =>(
             <li key={i} className="list-unstyled">
-                <input type="checkbox" className="form-check-input" />
+                <input  onChange={handleToggle(c._id)} value={checked.indexOf(c._id===-1)} type="checkbox" className="form-check-input" />
                 <label className="form-check-name">{c.name}</label>
             </li>
         ))
--- frontend/src/core/Shop.js
+++ frontend/src/core/Shop.js
@@ -3,10 +3,15 @@ import Layout from './Layout';
 import {getCategories} from './apiCore';
 import Card from  './Card';
 import CheckBox from './CheckBox';
+import RadioBox from './RadioBox';
+import prices from './fixedPrices';
 
 
 function Shop() {
 
+    const [myFilters, setMyFilters] = useState({
+        filters:{category:[], price:[]}
+    })
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(false);
 
@@ -23,17 +28,39 @@ function Shop() {
     useEffect(()=>{
         init()
     },[]);
+
+    const handleFilters = (filters, filtersBy) =>{
+        // console.log("SHOP",filters, filtersBy)
+
+        const newFilters = {...myFilters};
+        newFilters.filters[filtersBy] = filters;
+        setMyFilters(newFilters);
+    }
     return (
         <Layout title="Shop Page" description="React Node E-commerce Site">
             <div className="row">
                 <div className="col-4">
                     <h4>Filter by Categories</h4>
                     <ul>
-                        <  CheckBox categories={categories}/>
+                        <  CheckBox 
+                        categories={categories}
+                        handleFilters={filters =>{
+                            handleFilters(filters,'category')
+                        }}
+                        />
                     </ul>
+                    <h4>Filter by Prices</h4>
+                    <div>
+                        <  RadioBox 
+                        prices={prices}
+                        handleFilters={filters =>{
+                            handleFilters(filters,'prices')
+                        }}
+                        />
+                    </div>
                 </div>
-                <div className="col-4">
-                        content
+                <div className="col-8">
+                       {JSON.stringify(myFilters)}
                 </div>
             </div>
         </Layout>
